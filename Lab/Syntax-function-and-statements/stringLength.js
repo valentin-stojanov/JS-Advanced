b8d7cdb4404718c25c18e20f72b57d6e15267213
valentin <valentin_stojanov@abv.bg>
2021-09-12 13:20:59 +0300

    Lab: Syntax, Functions and Statements - added stringLength.js

---
+++ Lab/Syntax-function-and-statements/stringLength.js
@@ -0,0 +1,7 @@
+function stringLength(str1, str2, str3){
+    let length = str1.length + str2.length + str3.length;
+    console.log(length);
+    console.log(Math.floor(length / 3));
+}
+
+stringLength('pasta', '5', '22.3');
