d6ad96c30258c4d84480bd5a5de0a4bcaa4c715d
valentin-stojanov <valentin_stojanov@abv.bg>
2021-09-13 11:43:52 +0300

    Lab: Syntax, Functions and Statements - changed sumOfNumber.js

--- Lab/Syntax-function-and-statements/sumOfNumbers.js
+++ Lab/Syntax-function-and-statements/sumOfNumbers.js
@@ -1,13 +1,15 @@
 function sumOfNumbers(n, m) {
     n = Number(n);
     m = Number(m);
-    // const sum = (n + m) * (m - n + 1) / 2;
-    let sum = 0;
-    for (let i = n; n <= m; n++) {
-        sum += n;
-    }
+    
+    let sum = n <= m ? (n + m) * (m - n + 1) / 2 : 0;
+    
+    // let sum = 0;
+    // for (let i = n; n <= m; n++) {
+    //     sum += n;
+    // }
 
     console.log(sum);
 }
 
-sumOfNumbers(1, 10);
+sumOfNumbers(-5, -10);
