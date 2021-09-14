3119c9f5ded71bff48522a90eb5a8125ec4b232c
valentin-stojanov <valentin_stojanov@abv.bg>
2021-09-14 11:45:58 +0300

    Exercise: Syntax, Functions and Statements - formated sameNumber.js

--- Exercise/Syntax-Functions-and-Statements/sameNumber.js
+++ Exercise/Syntax-Functions-and-Statements/sameNumber.js
@@ -1,10 +1,10 @@
-function sameNumber(number){
+function sameNumber(number) {
     let strNumber = number.toString();
     let containsSameNumbers = true;
     let sumOfDigits = 0;
 
-    for(let i = 0; i < strNumber.length; i++){
-        if(strNumber[0] !== strNumber[i]){
+    for (let i = 0; i < strNumber.length; i++) {
+        if (strNumber[0] !== strNumber[i]) {
             containsSameNumbers = false;
         }
         sumOfDigits += Number(strNumber[i]);
