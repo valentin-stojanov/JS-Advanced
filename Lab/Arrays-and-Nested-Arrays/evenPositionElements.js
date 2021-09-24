191ca6a7b85c5ad95c2172e491899531f226bec9
valentin-stojanov <valentin_stojanov@abv.bg>
2021-09-24 10:45:42 +0300

    Lab: Arrays - added lasktKNumbersSecuence.js

--- Lab/Arrays-and-Nested-Arrays/evenPositionElements.js
+++ Lab/Arrays-and-Nested-Arrays/evenPositionElements.js
@@ -1,9 +1,12 @@
 function evenPositionElements(arr) {
-    const output = [];
-    for (let i = 0; i < arr.length; i += 2) {
-        output.push(arr[i]);
-    }
-    console.log(output.join(' '));
+    // const output = [];
+    // for (let i = 0; i < arr.length; i += 2) {
+    //     output.push(arr[i]);
+    // }
+    // console.log(output.join(' '));
+
+    console.log(arr.filter((e, i) => i % 2 == 0)
+    .join(' '));
 }
 
 evenPositionElements(['20', '30', '40', '50', '60']);
