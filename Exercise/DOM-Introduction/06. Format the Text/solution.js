7974a908843ac48d33ca0feeb6a88bf96bbddc2d
valentin <valentin_stojanov@abv.bg>
2021-10-09 20:09:32 +0300

    Exercises: DOM Introduction - added 06. Format the Text.

--- Exercise/DOM-Introduction/06. Format the Text/solution.js
+++ Exercise/DOM-Introduction/06. Format the Text/solution.js
@@ -1,3 +1,28 @@
 function solve() {
-  //TODO
+    let input = document.getElementById('input').value;
+    const sentencesArray = input
+        .split('.')
+        .filter(e => e.length !== 0)
+        .map(e => e + '.');
+
+
+    let resultToAppend = '';
+
+    let counter = 0;
+    for (let i = 0; i < sentencesArray.length; i++) {
+        resultToAppend += sentencesArray[i];
+        counter++
+        if (counter === 3) {
+            const paragraph = document.createElement('p');
+            paragraph.textContent = resultToAppend;
+            document.getElementById('output').appendChild(paragraph);
+            resultToAppend = '';
+            counter = 0;
+        }
+    }
+    if (resultToAppend !== '') {
+        const paragraph = document.createElement('p');
+        paragraph.textContent = resultToAppend;
+        document.getElementById('output').appendChild(paragraph);
+    }
 }
