fd9283b3e1a2ede46644aa6be11e25afde6b66c4
valentin <valentin_stojanov@abv.bg>
2021-10-09 14:23:26 +0300

    Exercises: DOM Introduction - added 05. Table - Search Engine.

--- Exercise/DOM-Introduction/05. Table - Search Engine/solution.js
+++ Exercise/DOM-Introduction/05. Table - Search Engine/solution.js
@@ -2,7 +2,18 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      const tableBody = Array.from(document.querySelectorAll("tbody tr"));
+      const input = document.querySelector('#searchField');
+      const searchText = input.value.toLowerCase();
 
+      for (const tableRow of tableBody) {
+         let row = tableRow.textContent.toLowerCase();
+         if (row.includes(searchText)){
+            tableRow.classList.add('select');
+         }else{
+            tableRow.classList.remove('select');
+         }
+      }
+      input.value = '';
    }
 }
