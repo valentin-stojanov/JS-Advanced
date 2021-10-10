bbd52a9030135776b2082124d3c7f75584ed5428
valentin <valentin_stojanov@abv.bg>
2021-10-10 19:09:13 +0300

    Lab: DOM-Manipulations-and-Events - added 02. Delete-From-Table.

--- Lab/DOM-Manipulations-and-Events/02. Delete-From-Table/app.js
+++ Lab/DOM-Manipulations-and-Events/02. Delete-From-Table/app.js
@@ -1,3 +1,21 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    const inputField = document.querySelector('input[name="email"]');
+    const searchText = inputField.value;
+
+    const rows = document.querySelectorAll('tbody tr');
+    let deletedRow = false;
+    for (const row of rows) {
+        const email = row.children[1];
+        if (searchText === email.textContent) {
+            row.remove();
+            deletedRow = true;
+        }
+    }
+    const resultElement = document.getElementById('result');
+
+    if (deletedRow) {
+        resultElement.textContent = 'Deleted'
+    } else {
+        resultElement.textContent = 'Not found.'
+    }
 }
