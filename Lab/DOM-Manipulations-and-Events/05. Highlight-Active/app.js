039e4cba57b0f927ee40e5036078484323bfe9c1
valentin <valentin_stojanov@abv.bg>
2021-10-10 22:16:03 +0300

    Lab: DOM-Manipulations-and-Events - added 05. Highlight-Active.

--- Lab/DOM-Manipulations-and-Events/05. Highlight-Active/app.js
+++ Lab/DOM-Manipulations-and-Events/05. Highlight-Active/app.js
@@ -1,3 +1,20 @@
 function focused() {
-    console.log('TODO:...');
+    const elements = Array.from(document.querySelectorAll('div div'));
+
+    for (const element of elements) {
+        const inputField = element.children[1];
+        inputField.addEventListener('focus', onFocus);
+        inputField.addEventListener('blur', onBlur);
+    }
+
+
+    function onFocus(ev){
+        const divElement = ev.target.parentNode;
+        divElement.className = 'focused';
+    }
+    function onBlur(ev){
+        const divElement = ev.target.parentNode;
+        divElement.className = 'blur';
+
+    }
 }
