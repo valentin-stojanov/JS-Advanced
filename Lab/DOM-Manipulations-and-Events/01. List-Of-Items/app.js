b88e5e745df62032d62f91c8f5ffb7eec7308989
valentin <valentin_stojanov@abv.bg>
2021-10-10 18:20:55 +0300

    Lab: DOM-Manipulations-and-Events - added 01. List-Of-Items.

--- Lab/DOM-Manipulations-and-Events/01. List-Of-Items/app.js
+++ Lab/DOM-Manipulations-and-Events/01. List-Of-Items/app.js
@@ -1,3 +1,12 @@
 function addItem() {
-    console.log('TODO:...');
+
+    const itemTextElement = document.querySelector('#newItemText');
+
+    const li = document.createElement('li');
+    li.textContent = itemTextElement.value;
+
+    const itemList = document.querySelector('#items');
+    itemList.appendChild(li);
+
+    itemTextElement.value = '';
 }
