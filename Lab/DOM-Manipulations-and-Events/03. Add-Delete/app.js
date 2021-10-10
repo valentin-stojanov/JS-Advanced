182145ac860d1c0c0f26929fcfcb5e40bf0a6e41
valentin <valentin_stojanov@abv.bg>
2021-10-10 20:03:40 +0300

    Lab: DOM-Manipulations-and-Events - added 03. Add-Delete.

--- Lab/DOM-Manipulations-and-Events/03. Add-Delete/app.js
+++ Lab/DOM-Manipulations-and-Events/03. Add-Delete/app.js
@@ -1,3 +1,22 @@
 function addItem() {
-    //TODO...
+    const itemTextElement = document.querySelector('#newItemText');
+
+    const button = document.createElement('a');
+    button.href = '#';
+    button.textContent = '[Delete]';
+    button.addEventListener('click', removeElement);
+
+    const li = document.createElement('li');
+    li.textContent = itemTextElement.value;
+    li.appendChild(button);
+
+    const itemList = document.querySelector('#items');
+    itemList.appendChild(li);
+
+    itemTextElement.value = '';
+
+    function removeElement(ev) {
+        const row = ev.target.parentNode;
+       row.remove();
+    }
 }
