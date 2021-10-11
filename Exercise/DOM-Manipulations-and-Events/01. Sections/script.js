3b8ccef453695e3de21860c01c65acabeff489b8
valentin <valentin_stojanov@abv.bg>
2021-10-11 22:11:56 +0300

    Exercises: DOM-Manipulations-and-Events - added 01. Sections.

--- Exercise/DOM-Manipulations-and-Events/01. Sections/script.js
+++ Exercise/DOM-Manipulations-and-Events/01. Sections/script.js
@@ -1,3 +1,18 @@
 function create(words) {
-   console.log('TODO:...');
+   const contentDiv = document.querySelector('#content');
+
+   for (const word of words) {
+      const div = document.createElement('div');
+      const p = document.createElement('p');
+      p.textContent = word;
+      p.style.display = 'none';
+      div.appendChild(p);
+      contentDiv.appendChild(div);
+   }
+
+   contentDiv.addEventListener('click', onClick);
+
+   function onClick(e){
+      e.target.firstChild.style.display = 'block';
+   }
 }
