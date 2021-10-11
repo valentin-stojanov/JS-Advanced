ef5c23d85ad4adf60a316403b8ac50985ca4b4eb
valentin <valentin_stojanov@abv.bg>
2021-10-11 14:50:19 +0300

    Lab: DOM-Manipulations-and-Events - added 07. Shopping-Cart.

--- Lab/DOM-Manipulations-and-Events/07. Shopping-Cart/solution.js
+++ Lab/DOM-Manipulations-and-Events/07. Shopping-Cart/solution.js
@@ -1,3 +1,45 @@
 function solve() {
-   //TODO...
+    const shoppingCardElement = document.querySelector('.shopping-cart');
+    shoppingCardElement.addEventListener('click', onClick);
+
+    const productsAndPrice = {};
+    let addedProducts = '';
+    const resultElement = document.querySelector('textarea');
+
+    function onClick(ev) {
+        const targetedElement = ev.target;
+        if (targetedElement.classList.contains('add-product')) {
+            const product = targetedElement.parentNode.parentNode;
+            const productName = product.querySelector('.product-title').textContent;
+            const productPrice = Number(product.querySelector('.product-line-price').textContent);
+            addedProducts += `Added ${productName} for ${productPrice.toFixed(2)} to the cart.\n`;
+
+            if (productsAndPrice[productName] === undefined) {
+                productsAndPrice[productName] = 0;
+            }
+
+            productsAndPrice[productName] += productPrice;
+            resultElement.textContent = addedProducts;
+
+        }
+
+        if (targetedElement.classList.contains('checkout')) {
+            let totalPrice = 0;
+            const productList = [];
+            Object.entries(productsAndPrice).forEach(entry => {
+                totalPrice += entry[1];
+                productList.push(entry[0]);
+            });
+            addedProducts += `You bought ${productList.join(', ')} for ${totalPrice.toFixed(2)}.\n`
+            resultElement.textContent = addedProducts;
+
+            targetedElement.parentNode
+                .querySelectorAll('button')
+                .forEach( b => b.disabled = true);
+        }
+
+        // resultElement.textContent = addedProducts;
+
+
+    }
 }
