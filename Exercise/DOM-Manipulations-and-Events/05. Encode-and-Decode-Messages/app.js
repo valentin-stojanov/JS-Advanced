f472020f6b53e49b3b2380948e30746dfda19195
valentin <valentin_stojanov@abv.bg>
2021-10-12 14:58:43 +0300

    Exercises: DOM-Manipulations-and-Events - added 05. Encode-and-Decode-Messages.

--- Exercise/DOM-Manipulations-and-Events/05. Encode-and-Decode-Messages/app.js
+++ Exercise/DOM-Manipulations-and-Events/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,39 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    const mainElement = document.querySelector('#main');
+    mainElement.addEventListener('click', encodeOrDecode);
+
+    function encodeOrDecode(e) {
+        const btn = e.target;
+        const message = mainElement
+            .querySelectorAll('div')[0]
+            .querySelector('textarea');
+        const lastReceivedMessage = mainElement
+            .querySelectorAll('div')[1]
+            .querySelector('textarea');
+
+        if (btn.textContent === 'Encode and send it') {
+            const messageText = message.value;
+            lastReceivedMessage.value = encode(messageText);
+            message.value = '';
+
+        } else if (btn.textContent === 'Decode and read it') {
+            lastReceivedMessage.value = decode(lastReceivedMessage.value);
+        }
+    }
+
+    function encode(message) {
+        const encodedAsciiSymbols = [];
+        for (let i = 0; i < message.length; i++) {
+            encodedAsciiSymbols.push(message.charCodeAt(i) + 1);
+        }
+        return String.fromCharCode(...encodedAsciiSymbols);
+    }
+
+    function decode(message) {
+        const decodedAsciiSymbols = [];
+        for (let i = 0; i < message.length; i++) {
+            decodedAsciiSymbols.push(message.charCodeAt(i) - 1);
+        }
+        return String.fromCharCode(...decodedAsciiSymbols);
+    }
 }
