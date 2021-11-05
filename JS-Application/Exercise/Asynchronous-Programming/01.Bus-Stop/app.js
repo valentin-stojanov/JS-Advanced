cb89dfe35c9fa22f072962a6d449f37eab2cb9e4
valentin-stojanov <valentin_stojanov@abv.bg>
2021-11-05 15:14:41 +0200

    Lab: Asynchronous Programming - added 01.Bus-Stop

--- JS-Application/Exercise/Asynchronous-Programming/01.Bus-Stop/app.js
+++ JS-Application/Exercise/Asynchronous-Programming/01.Bus-Stop/app.js
@@ -1,3 +1,34 @@
-function getInfo() {
-    console.log("TODO...");
+async function getInfo() {
+
+    const inputFieldelement = document.getElementById('stopId');
+    const stopId = inputFieldelement.value;
+    const stopNameElement = document.getElementById('stopName');
+    const busesElement = document.querySelector('#buses');
+
+    const url = `http://localhost:3030/jsonstore/bus/businfo/${stopId}`
+
+    try {
+        busesElement.replaceChildren();
+        const res = await fetch(url);
+        if (res.status != 200) {
+            throw new Error(res.status)
+        }
+
+        const data = await res.json();
+        stopNameElement.textContent = data.name;
+        const busArray = Object.entries(data.buses)
+
+        for (const busInfo of busArray) {
+            const lielement = document.createElement('li');
+            lielement.textContent = `Bus ${busInfo[0]} arrives in ${busInfo[1]} minutes`;
+            busesElement.appendChild(lielement);
+        }
+
+    } catch (error) {
+        stopNameElement.textContent = 'Error';
+    }
+
+
+
+
 }
