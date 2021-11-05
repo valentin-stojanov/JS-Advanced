185c48f8bed6705ba3b00d7139daa4793306df4d
valentin <valentin_stojanov@abv.bg>
2021-11-05 21:37:51 +0200

    JS-Application Exercise: Asynchronous-Programming - added 02.Bus-Schedule.

--- JS-Application/Exercise/Asynchronous-Programming/02.Bus-Schedule/app.js
+++ JS-Application/Exercise/Asynchronous-Programming/02.Bus-Schedule/app.js
@@ -1,11 +1,29 @@
 function solve() {
+    const label = document.querySelector(' #info span');
+    const departBtn = document.getElementById('depart');
+    const arriveBtn = document.getElementById('arrive');
+    let stop = {
+        next: 'depot'
+    }
 
-    function depart() {
-        console.log('Depart TODO...');
+    async function depart() {
+        departBtn.disabled = true;
+        arriveBtn.disabled = false;
+
+        const url = `http://localhost:3030/jsonstore/bus/schedule/${stop.next}`;
+
+        const res = await fetch(url);
+        stop = await res.json();
+
+        label.textContent = `Next stop ${stop.name}`;
+
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        arriveBtn.disabled = true;
+        departBtn.disabled = false;
+
+        label.textContent = `Arriving at ${stop.name}`;
     }
 
     return {
