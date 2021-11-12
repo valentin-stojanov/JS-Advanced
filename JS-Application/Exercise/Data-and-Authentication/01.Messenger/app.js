6ee15609c6eef081917a700908b20e300b3cd537
valentin-stojanov <valentin_stojanov@abv.bg>
2021-11-12 16:21:00 +0200

    Exercise: Data and Authentication - added 1.	Messenger(TBD).

--- JS-Application/Exercise/Data-and-Authentication/01.Messenger/app.js
+++ JS-Application/Exercise/Data-and-Authentication/01.Messenger/app.js
@@ -1,5 +1,25 @@
+const sendBtn = document.getElementById('submit');
+const refreshBtn = document.getElementById('refresh');
+const inputNameElement = document.querySelector('[name="author"]');
+const inputMessageElement = document.querySelector('[name="content"]');
+const textAreaElement = document.getElementById('messages');
+
 function attachEvents() {
-    console.log('TODO...');
+    refreshBtn.addEventListener('click', getMessages);
+    sendBtn.addEventListener('click', sengMessage);
 }
 
 attachEvents();
+
+async function getMessages(e) {
+    const url = 'http://localhost:3030/jsonstore/messenger';
+
+    const res = await fetch(url);
+    const data = await res.json();
+    const messages = Object.values(data);
+    textAreaElement.textContent = messages
+        .map((e) => `${e.author}: ${e.content}`)
+        .join('\n');
+}
+
+// async function sengMessage
