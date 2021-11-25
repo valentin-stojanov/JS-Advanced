42ba5b6ff351b462139795c235ed0c62e73e0252
valentin <valentin_stojanov@abv.bg>
2021-11-25 18:44:39 +0200

    JS-Application Exercise: SPA - 02.Movies (added like and unlike btn).

--- JS-Application/Exercise/SPA/02.Movies/src/details.js
+++ JS-Application/Exercise/SPA/02.Movies/src/details.js
@@ -1,4 +1,4 @@
-import { showView, e } from "./dom.js";
+import {showView, e} from "./dom.js";
 
 const section = document.getElementById('movie-details')
 section.remove();
@@ -12,59 +12,86 @@ async function getMovie(id) {
     section.replaceChildren(e('p', {}, 'Loading...'));
 
     const requests = [
-         fetch('http://localhost:3030/data/movies/' + id),
-         fetch(`http://localhost:3030/data/likes?where=movieId%3D%22${id}%22&distinct=_ownerId&count`)
+        fetch('http://localhost:3030/data/movies/' + id),
+        fetch(`http://localhost:3030/data/likes?where=movieId%3D%22${id}%22&distinct=_ownerId&count`)
     ]
 
     const userData = JSON.parse(sessionStorage.getItem('userData'));
 
-    if(userData != null){
-        requests.push(fetch(`http://localhost:3030/data/likes?where=movieId%3D%22${id}%22%20and%20_ownerId%3D%22${userData.id}%22&count`))
+    if (userData != null) {
+        requests.push(fetch(`http://localhost:3030/data/likes?where=movieId%3D%22${id}%22%20and%20_ownerId%3D%22${userData.id}%22`))
     }
 
     const [movieRes, likesRes, hasLikedRes] = await Promise.all(requests);
 
     const [movieData, likesData, hasLiked] = await Promise.all([
-         movieRes.json(),
-         likesRes.json(),
-         hasLikedRes && hasLikedRes.json()
+        movieRes.json(),
+        likesRes.json(),
+        hasLikedRes && hasLikedRes.json()
     ]);
-    
+
     section.replaceChildren(createDetails(movieData, likesData, hasLiked));
 }
 
 function createDetails(movie, likes, hasLiked) {
-    const controls = e('div', { className: 'col-md-4 text-center' },
-        e('h3', { className: 'my-3' }, 'Movie Description'),
+    const controls = e('div', {className: 'col-md-4 text-center'},
+        e('h3', {className: 'my-3'}, 'Movie Description'),
         e('p', {}, movie.description)
     );
 
     const userData = JSON.parse(sessionStorage.getItem('userData'));
     if (userData != null) {
         if (userData.id == movie._ownerId) {
-            controls.appendChild(e('a', { className: 'btn btn-danger', href: '#' }, 'Delete'));
-            controls.appendChild(e('a', { className: 'btn btn-warning', href: '#' }, 'Edit'));
+            controls.appendChild(e('a', {className: 'btn btn-danger', href: '#'}, 'Delete'));
+            controls.appendChild(e('a', {className: 'btn btn-warning', href: '#'}, 'Edit'));
         } else {
-            if(hasLiked){
-                controls.appendChild(e('a', { className: 'btn btn-primary', href: '#' }, 'Unlike'));
+            if (hasLiked.length > 0) {
+                controls.appendChild(e('a', {className: 'btn btn-primary', href: '#', onClick: onUnlike}, 'Unlike'));
             } else {
-                controls.appendChild(e('a', { className: 'btn btn-primary', href: '#' }, 'Like'));
+                controls.appendChild(e('a', {className: 'btn btn-primary', href: '#', onClick: onLike}, 'Like'));
             }
-            
+
         }
     }
 
-    controls.appendChild(e('span', { className: 'enrolled-span'}, `Liked ${likes}`));
+    controls.appendChild(e('span', {className: 'enrolled-span'}, `Liked ${likes}`));
 
-    const element = e('div', { className: 'container' },
-        e('div', { className: 'row bg-light text-dark' },
+    const element = e('div', {className: 'container'},
+        e('div', {className: 'row bg-light text-dark'},
             e('h1', {}, `Movie title: ${movie.title}`),
-            e('div', { className: 'col-md-8' },
-                e('img', { className: 'img-thumbnail', src: movie.img, alt: 'Movie' })
+            e('div', {className: 'col-md-8'},
+                e('img', {className: 'img-thumbnail', src: movie.img, alt: 'Movie'})
             ),
             controls
         )
     );
 
     return element
+
+    async function onLike() {
+        const res = await fetch('http://localhost:3030/data/likes', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Authorization': userData.token
+            },
+            body: JSON.stringify({
+                movieId: movie._id
+            })
+        });
+
+        showDetails(movie._id);
+    }
+
+    async function onUnlike() {
+        const likeId = hasLiked[0]._id;
+        const res = await fetch('http://localhost:3030/data/likes/' + likeId, {
+            method: 'delete',
+            headers: {
+                'X-Authorization': userData.token
+            },
+        });
+
+        showDetails(movie._id);
+    }
 }
