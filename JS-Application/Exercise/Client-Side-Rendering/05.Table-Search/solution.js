f6a0966baa266a555c012aa92d2169a9eba3270c
valentin <valentin_stojanov@abv.bg>
2021-12-05 20:17:35 +0200

    JS Applications Client Side Rendering Exercise - added 5.Table – Search Engine.

--- JS-Application/Exercise/Client-Side-Rendering/05.Table-Search/solution.js
+++ JS-Application/Exercise/Client-Side-Rendering/05.Table-Search/solution.js
@@ -1,8 +1,42 @@
-function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+import {html, render} from "./node_modules/lit-html/lit-html.js";
 
-   function onClick() {
-      //   TODO:
+const tbody = document.querySelector('tbody');
+const input = document.querySelector(`#searchField`);
+document.querySelector(`#searchBtn`).addEventListener('click', onSearch);
 
-   }
+const rowTemplate = (studentData) => html`
+    <tr class=${studentData.match ? 'select' : ''}>
+        <td>${studentData.student.firstName} ${studentData.student.lastName}</td>
+        <td>${studentData.student.email}</td>
+        <td>${studentData.student.course}</td>
+    </tr>`;
+
+let studentsData;
+
+async function getData(){
+    const url = `http://localhost:3030/jsonstore/advanced/table`;
+
+    const res = await fetch(url);
+    const data = Object.values(await res.json());
+    studentsData = data.map(s => ({student: s, match: false}));
+
+    update();
 }
+
+getData();
+
+function update(){
+    render(studentsData.map(rowTemplate), tbody)
+}
+
+function onSearch(ev){
+    const searchText = input.value.toLocaleLowerCase();
+
+    studentsData.forEach(s => searchText && Object.values(s.student)
+        .join('')
+        .toLocaleLowerCase()
+        .includes(searchText) ? s.match = true : s.match = false);
+
+    update()
+}
+
