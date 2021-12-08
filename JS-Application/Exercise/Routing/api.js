1bbf3b62a87f1bf6b241d0b60a508bbaf94b57f4
valentin-stojanov <valentin_stojanov@abv.bg>
2021-12-08 11:29:23 +0200

    JS Application Exercise Routing - added data.js and api.js

--- JS-Application/Exercise/Routing/api.js
+++ JS-Application/Exercise/Routing/api.js
@@ -1,6 +1,8 @@
+const host = 'http://localhost:3030';
+
 async function request(url, options){
     try{
-    const response = await fetch (url, options)
+    const response = await fetch (host + url, options)
 
     if (response.ok != ok){
         if(response.status == 403){
@@ -26,5 +28,63 @@ function createOptions(method = 'get', data){
     const options = {
         method,
         headers: {}
+    };
+
+    if(data != undefined){
+        options.headers['Content-Type'] = 'application/json';
+        options.body = JSON.stringify(data);
     }
+
+    const userData = JSON.parse(sessionStorage.getItem('userData'));
+
+    if (userData != null){
+        options.headers['X-Authorization'] = userData.token;
+    }
+
+    return options;
+}
+
+export async function get(url){
+    return request(url);
+}
+
+export async function  post(url, data){
+    return request(url, createOptions('post', data));
+}
+
+export async function put(url, data){
+    return request(url, createOptions('put', data));
+}
+
+export async function del(url){
+    return request(url, createOptions('delete'));
+}
+
+export async function login(email, password){
+    const result = await post('/users/login', {email, password} );
+
+    const userData = {
+        email: result.email,
+        id: result._id,
+        token: result.accessToken
+    }
+
+    sessionStorage.setItem('userData', JSON.stringify(userData));
+}
+
+export async function register(email, password){
+    const result = await post('/users/register', {email, password} );
+
+    const userData = {
+        email: result.email,
+        id: result._id,
+        token: result.accessToken
+    }
+
+    sessionStorage.setItem('userData', JSON.stringify(userData));
+}
+
+export async function logout(){
+    await get('/users/logout');
+    sessionStorage.removeItem('userData');
 }
