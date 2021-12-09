a164340eca951ca9aea27f38a756afe852e9e905
valentin-stojanov <valentin_stojanov@abv.bg>
2021-12-09 10:49:18 +0200

    JS Application Exercise - Routing

---
+++ JS-Application/Exercise/Routing/01.Furniture/src/lib.js
@@ -0,0 +1,11 @@
+import {render, html} from '../node_modules/lit-html/lit-html.js';
+import {until} from '../node_modules/lit-html/directives/until.js';
+import {page} from '../node_modules/page/page.mjs';
+
+
+export {
+    html,
+    render,
+    until,
+    page
+};
